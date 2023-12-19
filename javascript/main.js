eb94a1c7b99b9e50c1970e6ed7983a395c51da3d
devgme <devang.goldmedalindia@gmail.com>
2023-12-19 11:27:18 +0530

    image uplaod

--- javascript/main.js
+++ javascript/main.js
@@ -389,5 +389,5 @@
   
   /* random color Animation ------------------------------------------------------------------------------------- */
   // $(document).ready(function() {
-  //   $("body").css("background-color", "hsla(" + Math.floor(Math.random() * (360)) + ", 75%, 58%, 1)");
+  //   $("---").css("background-color", "hsla(" + Math.floor(Math.random() * (360)) + ", 75%, 58%, 1)");
   // });
