83510ee3a87475fd450a956e4b066fab503d0525
devangpradhan <>
2024-02-05 16:01:39 +0530

    upload

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,11 +1,18 @@
 
 
-
-  $(document).ready(function() {
-    // Generate a random HSL color
-    var randomHSLColor = "hsla(" + Math.floor(Math.random() * 360) + ", 75%, 65%, 5)";
-    
+// -------------------------- Background Refrash
+document.addEventListener("DOMContentLoaded", function() {
+    // Array of possible colors
+    const colors = ["#B4D700", "#3498db", "#e74c3c", "#2ecc71", "#f39c12", "#9b59b6", "#ffb400","#f08f74","#ff4a54","#55e6a5","#5b78f6"];
+    // Get the element to change its color
+    const colorChangeElement = document.getElementById("colorChangeElement");
   
-    // Set the custom CSS property '--primary-bg-light' on the root element
-    document.documentElement.style.setProperty('--primary-bg-light', randomHSLColor);
+    // Generate a random index to pick a color from the array
+    const randomIndex = Math.floor(Math.random() * colors.length);
+  
+    // Set the --primary-bg-light variable to the selected color
+    document.documentElement.style.setProperty('--primary-bg-light', colors[randomIndex]);
   });
+
+
+// -------------------------- ASCII Effects
